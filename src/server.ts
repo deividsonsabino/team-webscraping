893c8e0f3d059df764b0aab3d708ccb9524ce038
deividsonsabino <deividsonsabino@outlook.com.br>
2024-03-18 14:08:16 -0300

    :tada: first commit

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+
+import express from 'express'
+import { Router, Request, Response } from 'express'
+
+const app = express()
+
+const route = Router()
+
+app.use(express.json()) 
+
+route.get('/', (req, res) => {
+    res.json({ message: 'Hello world with Typescript' })
+ })
+
+app.use(route)
+ 
+app.listen(3333, () => {
+    'server running on port 3333'
+})
